018371941013144467eb71733d4452a823d080f9
JadenCooper <jaden_cooper@outlook.com>
2022-03-30 09:38:32 +1300

    Completed Third Lecture Tasks

--- Tower Defence/Assets/Scripts/GameManagerBehaviour.cs
+++ Tower Defence/Assets/Scripts/GameManagerBehaviour.cs
@@ -11,6 +11,48 @@ public class GameManagerBehaviour : MonoBehaviour
     public GameObject[] nextWaveLabels;
     public bool gameOver = false;
     private int wave;
+    public Text healthLabel;
+    public GameObject[] healthIndicator;
+    private int health;
+    void Start()
+    {
+        Gold = 1000;
+        Wave = 0;
+        Health = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public int Health
+    {
+        get { return health; }
+        set
+        {
+            if (value < health)
+                Camera.main.GetComponent<CameraShake>().Shake();
+
+            health = value;
+            healthLabel.text = "HEALTH: " + health;
+
+            if (health <= 0 && !gameOver)
+            {
+                gameOver = true;
+                GameObject gameOverText = GameObject.FindGameObjectWithTag("GameOver");
+                gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+            }
+
+            for (int i = 0; i < healthIndicator.Length; i++)
+            {
+                if (i < Health)
+                    healthIndicator[i].SetActive(true);
+                else
+                    healthIndicator[i].SetActive(false);
+            }
+        }
+    }
     public int Wave
     {
         get { return wave; }
@@ -38,16 +80,5 @@ public class GameManagerBehaviour : MonoBehaviour
             goldLabel.GetComponent<Text>().text = "GOLD: " + gold;
         }
 
-    }
-    void Start()
-    {
-        Gold = 1000;
-        Wave = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
